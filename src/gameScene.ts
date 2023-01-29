d46fb1f915d0e6df9c01ac8218483e87c442403c
Adam Due Hansen <adamduehan@gmail.com>
2023-01-29 10:31:08 +0100

    Adds speed comp.

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -8,11 +8,9 @@ import {
 } from 'kaboom';
 import combineColors, { ColorChannels } from './combineColors';
 import kctx from './kctx';
+import { speed, SpeedComp } from './speedComp';
 
-type Player = GameObj<RectComp | ColorComp | PosComp | OriginComp>;
-
-let ySpeed = 0;
-let xSpeed = 0;
+type Player = GameObj<RectComp | ColorComp | PosComp | OriginComp | SpeedComp>;
 
 function getColorChannels(color: Color): ColorChannels {
   const { r, g, b } = color;
@@ -23,45 +21,13 @@ function getColorChannels(color: Color): ColorChannels {
   };
 }
 
-function updateSpeedByControl() {
-  if (kctx.isKeyDown('up')) {
-    ySpeed -= 0.05;
-  }
-  if (kctx.isKeyDown('right')) {
-    xSpeed += 0.05;
-  }
-  if (kctx.isKeyDown('down')) {
-    ySpeed += 0.05;
-  }
-  if (kctx.isKeyDown('left')) {
-    xSpeed -= 0.05;
-  }
-}
-
-function resetToMaxSpeed(): void {
-  if (xSpeed > 2) {
-    xSpeed = 2;
-  }
-
-  if (xSpeed < -2) {
-    xSpeed = -2;
-  }
-
-  if (ySpeed > 2) {
-    ySpeed = 2;
-  }
-
-  if (ySpeed < -2) {
-    ySpeed = -2;
-  }
-}
-
 function gameScene(): void {
   const player: Player = kctx.add([
     kctx.rect(16, 16),
     kctx.color(kctx.WHITE),
     kctx.pos(kctx.width() / 2, kctx.height() / 2),
     kctx.origin('center'),
+    speed(),
     'player',
   ]);
 
@@ -82,10 +48,19 @@ function gameScene(): void {
   kctx.onKeyPress('b', makeAddPlayerColorHandler(kctx.BLUE));
   kctx.onKeyPress('w', makeAddPlayerColorHandler(kctx.WHITE));
 
-  onUpdate(updateSpeedByControl);
-  onUpdate(resetToMaxSpeed);
   onUpdate(() => {
-    player.moveBy(xSpeed, ySpeed);
+    if (kctx.isKeyDown('up')) {
+      player.ySpeed -= 0.05;
+    }
+    if (kctx.isKeyDown('right')) {
+      player.xSpeed += 0.05;
+    }
+    if (kctx.isKeyDown('down')) {
+      player.ySpeed += 0.05;
+    }
+    if (kctx.isKeyDown('left')) {
+      player.xSpeed -= 0.05;
+    }
   });
 }
 
