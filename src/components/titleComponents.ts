7b950f9b350ff8d236867bd387c6ee1c732832ed
Adam Due Hansen <adamduehan@gmail.com>
2023-02-11 10:06:17 +0100

    Title is now random colorized.

--- src/components/titleComponents.ts
+++ src/components/titleComponents.ts
@@ -1,13 +1,14 @@
 import { Comp } from 'kaboom';
 import kctx from '../kctx';
+import colorizedText from './custom/colorizedText';
 
 const titleComponents: Comp[] = [
-  kctx.text('COLOROIDS', {
+  colorizedText('COLOROIDS', {
     size: 64,
     font: 'sink',
   }),
   kctx.origin('center'),
-  kctx.pos(kctx.width() / 2, 200),
+  kctx.pos(kctx.width() / 2, 150),
 ];
 
 export default titleComponents;
