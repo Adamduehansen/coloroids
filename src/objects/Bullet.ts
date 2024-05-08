5956f213ec0204dc0cf66bb5ab3251fc07f08b1d
Adam Due Hansen <adamduehan@gmail.com>
2024-05-08 07:53:30 +0200

    Kills bullet when offscreen.

--- src/objects/Bullet.ts
+++ src/objects/Bullet.ts
@@ -1,5 +1,7 @@
 import { Actor, ActorArgs, vec } from "excalibur";
 
+const BULLET_SPEED = 500;
+
 export default class Bullet extends Actor {
   constructor(args: ActorArgs) {
     super({
@@ -11,8 +13,19 @@ export default class Bullet extends Actor {
   }
 
   onInitialize(): void {
-    const vx = Math.cos(this.rotation) * 500;
-    const vy = Math.sin(this.rotation) * 500;
+    const vx = Math.cos(this.rotation) * BULLET_SPEED;
+    const vy = Math.sin(this.rotation) * BULLET_SPEED;
     this.vel = vec(vx, vy);
+  }
+
+  onPreUpdate(): void {
+    this.#killIfOffScreen();
+  }
+
+  #killIfOffScreen() {
+    if (!this.isOffScreen) {
+      return;
+    }
+    this.kill();
   }
 }
