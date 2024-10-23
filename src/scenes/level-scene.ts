2a386c9e4d106d9c240dbf1c5b40fc0d63088952
Adam Due Hansen <adamduehan@gmail.com>
2024-10-23 07:00:50 +0200

    feat(Level): asteroids can be loaded through Tiled.

--- src/scenes/level-scene.ts
+++ src/scenes/level-scene.ts
@@ -1,5 +1,7 @@
 import * as ex from "excalibur";
 import type { FactoryProps, TiledResource } from "@excaliburjs/plugin-tiled";
+import { SmallAsteroid } from "../actors/small-asteroid.ts";
+import { LargeAsteroid } from "../actors/large-asteroid.ts";
 
 interface Args {
   tilemap: TiledResource;
@@ -8,10 +10,7 @@ interface Args {
 export class LevelScene extends ex.Scene {
   #tilemap: TiledResource;
 
-  #entityFactory: Record<
-    string,
-    (props: FactoryProps) => ex.Actor | undefined
-  > = {
+  #entityFactory: Record<string, (props: FactoryProps) => ex.Actor> = {
     Text: function (props) {
       return new ex.Label({
         x: props.object?.x ?? 0,
@@ -22,6 +21,32 @@ export class LevelScene extends ex.Scene {
         collisionType: ex.CollisionType.PreventCollision,
         z: props.layer.order ?? 0,
         color: ex.Color.White,
+      });
+    },
+    SmallAsteroid: function (props): ex.Actor {
+      const color = props.object?.properties.get("color")?.toString() ??
+        "ffffff";
+
+      return new SmallAsteroid({
+        vel: ex.vec(
+          ex.randomIntInRange(-50, 50),
+          ex.randomIntInRange(-50, 50),
+        ),
+        color: ex.Color.fromHex(color),
+        pos: props.worldPos,
+      });
+    },
+    LargeAsteroid: function (props): ex.Actor {
+      const color = props.object?.properties.get("color")?.toString() ??
+        "ffffff";
+
+      return new LargeAsteroid({
+        vel: ex.vec(
+          ex.randomIntInRange(-50, 50),
+          ex.randomIntInRange(-50, 50),
+        ),
+        color: ex.Color.fromHex(color),
+        pos: props.worldPos,
       });
     },
   };
--- src/scenes/test-level.ts
+++ src/scenes/test-level.ts
@@ -2,8 +2,6 @@ import * as ex from "excalibur";
 import { Resources } from "../utils/resources.ts";
 import { LevelScene } from "./level-scene.ts";
 import { Spaceship } from "../actors/spaceship.ts";
-import { SmallAsteroid } from "../actors/small-asteroid.ts";
-import { LargeAsteroid } from "../actors/large-asteroid.ts";
 
 export class TestLevel extends LevelScene {
   constructor() {
@@ -20,20 +18,6 @@ export class TestLevel extends LevelScene {
       color: ex.Color.Green,
     });
     engine.add(spaceship);
-
-    const smallAsteroid = new SmallAsteroid({
-      pos: ex.vec(500, 300),
-      color: ex.Color.Green,
-      vel: ex.vec(-30, 0),
-    });
-    engine.add(smallAsteroid);
-
-    const largeAsteroid = new LargeAsteroid({
-      pos: ex.vec(400, 300),
-      color: ex.Color.Yellow,
-      vel: ex.vec(50, 0),
-    });
-    engine.add(largeAsteroid);
 
     engine.currentScene.camera.strategy.lockToActor(spaceship);
     engine.currentScene.camera.zoom = 1.5;
