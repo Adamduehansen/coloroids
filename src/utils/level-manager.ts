0f9b7fa5f73620528a8d148eb3a9da781f576f99
Adam Due Hansen <adamduehan@gmail.com>
2024-10-29 07:16:32 +0100

    feat(Level): adds transition animation.

--- src/utils/level-manager.ts
+++ src/utils/level-manager.ts
@@ -1,20 +1,41 @@
 import * as ex from "excalibur";
 
+type LevelKey = `level-${number}`;
+
 export class LevelManager {
   #engine: ex.Engine;
-  #currentLevelKey: string;
+  #currentLevelKey: LevelKey;
 
   constructor(engine: ex.Engine) {
     this.#engine = engine;
 
     this.#currentLevelKey = "level-1";
 
-    this.#engine.on("level-transition", () => {
-      ex.Logger.getInstance().info("Transition");
-    });
+    this.#engine.on("level-transition", this.#goToNextLevel.bind(this));
   }
 
   get getCurrentLevel(): string {
     return this.#currentLevelKey;
+  }
+
+  #getNextLevel(): LevelKey {
+    return "level-2";
+  }
+
+  #goToNextLevel(): void {
+    const nextLevel = this.#getNextLevel();
+    ex.Logger.getInstance().info("Transition to", nextLevel);
+    this.#engine.goToScene(nextLevel, {
+      destinationIn: new ex.FadeInOut({
+        duration: 500,
+        direction: "in",
+        color: ex.Color.Black,
+      }),
+      sourceOut: new ex.FadeInOut({
+        duration: 500,
+        direction: "out",
+        color: ex.Color.Black,
+      }),
+    });
   }
 }
